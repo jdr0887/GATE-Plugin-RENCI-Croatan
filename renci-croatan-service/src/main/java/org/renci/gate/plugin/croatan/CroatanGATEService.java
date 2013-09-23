831e3ce66bfae0aa3047034f158a283a87a41283
Jason Reilly <jdr0887@renci.org>
2013-09-23 09:49:36 -0400

    queue filtering happens in parent api

--- renci-croatan-service/src/main/java/org/renci/gate/plugin/croatan/CroatanGATEService.java
+++ renci-croatan-service/src/main/java/org/renci/gate/plugin/croatan/CroatanGATEService.java
@@ -100,11 +100,6 @@ public class CroatanGATEService extends AbstractGATEService {
     public void createGlidein(Queue queue) throws GATEException {
         logger.info("ENTERING createGlidein(Queue)");
 
-        if (StringUtils.isNotEmpty(getActiveQueues()) && !getActiveQueues().contains(queue.getName())) {
-            logger.warn("queue name is not in active queue list...see etc/org.renci.gate.plugin.croatan.cfg");
-            return;
-        }
-
         File submitDir = new File("/tmp", System.getProperty("user.name"));
         submitDir.mkdirs();
 
